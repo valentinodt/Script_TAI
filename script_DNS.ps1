9d7e6bd7fc59e4f538a0692d682ce7f47994612c
Valentin Odinotte <153813866+valentinodt@users.noreply.github.com>
2024-03-12 14:52:09 +0100

    a

--- script_DNS.ps1
+++ script_DNS.ps1
@@ -1,6 +1,5 @@
 # Définition des paramètres IP statiques
 $ipv4Address = "192.168.1.100"
-$subnetMask = "255.255.255.0"
 $defaultGateway = "192.168.1.1"
 $dnsServer = "127.0.0.1" # Adresse IP du serveur DNS local
 
