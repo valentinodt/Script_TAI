709c4212926dd5d9b6236e7615373c950946fa29
Valentin Odinotte <153813866+valentinodt@users.noreply.github.com>
2024-03-12 13:54:46 +0100

    a

--- script_DNS.ps1
+++ script_DNS.ps1
@@ -7,3 +7,8 @@ $dnsServer = "127.0.0.1" # Adresse IP du serveur DNS local
 # Configuration de l'adresse IP statique sur la carte réseau
 Write-Output "Configuration de l'adresse IP statique sue la carte réseau..."
 New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $ipv4Address -Prefix 24 -DefaultGateway $defaultGateway
+
+
+# Configuration des serveurs DNS
+Write-Output "Configuration des serveurs DNS..."
+Set-DnsClientServerAdress -InterfaceAlias "Ethernet" -ServerAddresses $dnsServer
